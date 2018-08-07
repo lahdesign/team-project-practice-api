7e2b9f1e89d37b5da32f430b7c5b722c156f72fe
rachael miller design <rachael@rachaelmillerdesign.com>
2018-08-07 14:08:05 -0400

    added pageSchema

---
+++ app/models/page.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const pageSchema = new mongoose.Schema({
+
+  photo: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  title: {
+    type: String,
+  },
+  blogId: {
+     type: mongoose.Schema.Types.ObjectId,
+     ref: "Blog",
+     required: true
+   }
+})
+
+module.exports = mongoose.model('Page', userSchema)
